f983e5f1a39319c2f1e9de9f7793380f0b4e6580
Ankan Sharma <ankanroni3@gmail.com>
2021-06-20 19:05:13 +0530

    Used mediapipe holistic to get keypoints

--- App/mediapipe_kp.py
+++ App/mediapipe_kp.py
@@ -11,19 +11,23 @@ mp_drawing = mp.solutions.drawing_utils
 
 def mediapipe_detection(image,model):
     image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-    image.flags.writiable = False
+    image.flags.writeable = False
     results = model.process(image)
-    image.flags.writable = True
+    image.flags.writeable = True
     image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
     return image, results
 
 # Access webcam using opencv
 cap = cv2.VideoCapture(0)
-while cap.isOpened():
-    #read feed
-    ret, frame = cap.read()
-    cv2.imshow('OpenCv Feed', frame)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+with mp_holistic.Holistic(min_detection_confidence = 0.5, min_tracking_confidence = 0.5) as holistic:
+    while cap.isOpened():
+        #read feed
+        ret, frame = cap.read()
+        # Make prediction
+        image,results = mediapipe_detection(frame, holistic)
+        print(results)
+        cv2.imshow('OpenCv Feed', frame)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
