d6c6a2704c5350fe61a6063f869ce744e8713d08
Ankan Sharma <ankanroni3@gmail.com>
2021-06-25 18:08:15 +0530

    Updated design in script

--- App/mediapipe_kp.py
+++ App/mediapipe_kp.py
@@ -29,6 +29,37 @@ def draw_on_prediction(image,results):
     mp_drawing.draw_landmarks(
         image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
 
+# Draw landmark with formatting
+def draw_formatted_landmark(image,results):
+    mp_drawing.draw_landmarks(
+    image, 
+    results.face_landmarks, 
+    mp_holistic.FACE_CONNECTIONS,
+    mp_drawing.DrawingSpec(color=(80,120,50), thickness =1, circle_radius=1),
+    mp_drawing.DrawingSpec(color=(90,200,111), thickness =1, circle_radius=1)
+    )
+    mp_drawing.draw_landmarks(
+    image, 
+    results.left_hand_landmarks, 
+    mp_holistic.HAND_CONNECTIONS,
+    mp_drawing.DrawingSpec(color=(40,10,210), thickness =2, circle_radius=2),
+    mp_drawing.DrawingSpec(color=(180,96,72), thickness =2, circle_radius=2)
+    )
+    mp_drawing.draw_landmarks(
+    image, 
+    results.right_hand_landmarks, 
+    mp_holistic.HAND_CONNECTIONS,
+    mp_drawing.DrawingSpec(color=(40,10,210), thickness =2, circle_radius=2),
+    mp_drawing.DrawingSpec(color=(180,96,72), thickness =2, circle_radius=2)
+    )
+    mp_drawing.draw_landmarks(
+    image, 
+    results.pose_landmarks, 
+    mp_holistic.POSE_CONNECTIONS,
+    mp_drawing.DrawingSpec(color=(130,140,10), thickness =2, circle_radius=2),
+    mp_drawing.DrawingSpec(color=(80,36,14), thickness =2, circle_radius=2)
+    )
+
 # Access webcam using opencv
 def main_func():
     cap = cv2.VideoCapture(0)
@@ -42,7 +73,10 @@ def main_func():
             image, results = mediapipe_detection(frame, holistic)
 
             # Draw Connections
-            draw_on_prediction(image,results)
+            # draw_on_prediction(image,results)
+
+            # Draw formatted Landmark
+            draw_formatted_landmark(image,results)
 
             #print(results)
             cv2.imshow("'Holistic feed", image)
